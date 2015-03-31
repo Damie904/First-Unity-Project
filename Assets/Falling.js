15e701555cf23bca23b9e1cdd6afa820a8b15cc8
Damien McKie <damie904@gmail.com>
2015-03-31 15:15:53 -0400

    Initial Pull

---
+++ Assets/Falling.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+public var moveSpeed : float = -1f;
+public var turnSpeed : float = 0.1f;
+public var counter : int = 0;
+//public var supah : int = 0;
+//var coll: Collider;
+
+
+function Update ()
+{
+        counter++;
+        Counter();
+        
+}
+
+function Counter()
+{
+
+        if(counter < 600)
+        {
+        transform.Translate(new Vector3(counter/100,moveSpeed,0));
+        }
+        else
+        {
+        Physics.gravity = Vector3(0, 3.0, 0);
+        yield;
+        }
+}
---
+++ Assets/Squishsquash.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 0.1f;
+public var counter : int = 0;
+
+function Update ()
+{
+        
+        counter++;
+        Counter();
+}
+function Counter()
+{
+
+        if(counter < 300)
+        {
+        transform.Rotate(new Vector3(0,turnSpeed,0));
+        }
+        else
+        {
+        transform.Rotate(new Vector3(turnSpeed,0,0));
+        transform.Translate(new Vector3(0,turnSpeed,0));
+        yield;
+        }
+}
